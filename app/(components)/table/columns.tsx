ff449ab9b5d04475520e40edb91dc028b7220163
facsert <facsert@outlook.com>
2024-05-22 22:26:26 +0800

    react
    1.add table column setting
    2.add table sort
    
    Signed-off-by: facsert <facsert@outlook.com>

---
+++ app/(components)/table/columns.tsx
@@ -0,0 +1,62 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+export type Student = {
+    id: number;
+    name: string;
+    age: number;
+    sex: "boy"|"girl";
+    create_at: string;
+}
+
+// 定义列的标题和内容的样式
+export const columns: ColumnDef<Student>[] = [
+    {
+        accessorKey: "name",
+        header: () => <div className="text-left">Name</div>,
+        cell: ({ row }) => <div className="text-left font-medium">{row.getValue("name")}</div>,
+    },
+    {
+        accessorKey: "age",
+        header: ({column}) => {
+            return (
+                <div>
+                    <Button
+                        variant="ghost"
+                        className="cursor-pointer w-full flex flex-row justify-start items-center p-2"
+                        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                    >   
+                        <p>Age</p>
+                        <ArrowUpDown className="ml-2 h-4 w-4" />
+                    </Button>
+                </div>
+            )
+        },
+        cell: ({row}) => <div className="text-left px-4">{row.getValue("age")}</div>
+        // header: 
+    },
+    {
+        accessorKey: "sex",
+        header: "性别",
+        cell: ({ row }) => <div className="text-left">{row.getValue("sex") === "boy" ? "男生" : "女生"}</div>,
+    },
+    {
+        accessorKey: "create_at",
+        header: "Create At",
+    },
+    {
+        id: "actions",
+        cell: ({ row }) => {
+            const student = row.original
+            return (
+                <div className="flex flex-row gap-4 ">
+                    <button>Edit</button>
+                    <button>Delete</button>
+                </div>
+            )
+        }
+    }
+]
--- app/(components)/table/page.tsx
+++ app/(components)/table/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import {
     Table,
     TableBody,
@@ -8,58 +9,75 @@ import {
 } from "@/components/ui/table";
 
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { DataTable } from "./table";
+import { columns, Student } from "./columns";
 
-import { getStudents, Student } from "@/hook/student";
+const students: Student[] = [
+    {
+        id: 1,
+        name: "John Doe",
+        age: 20,
+        sex: "boy",
+        create_at: "20240512",
+    },
+    {
+        id: 2,
+        name: "Jane Doe",
+        age: 21,
+        sex: "girl",
+        create_at: "20240512",
+    },
+    {
+        id: 3,
+        name: "Jack Doe",
+        age: 19,
+        sex: "boy",
+        create_at: "20240512",
+    },
+    {
+        id: 3,
+        name: "Lily Doe",
+        age: 20,
+        sex: "girl",
+        create_at: "20240512",
+    }
+]
 
-
-export default async function TablePage() {
-    // const data:Student[] = await getStudents();
-    const data:Student[] = [];
+export default function TablePage() {
+    // const data:Student[] = [];
     return (
        <div className="flex flex-col justify-start items-center h-full w-full">
             <div className="w-full h-[40px] text-center">
                 <h1>Student Table</h1>
             </div>
             <ScrollArea className="w-full h-5/6 rounded-md border">
-            <Table>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead>Id</TableHead>
-                        <TableHead>Name</TableHead>
-                        <TableHead>Email</TableHead>
-                        <TableHead>Create_at</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    { data.length === 0 ? Empty() : List(data) }
-                </TableBody>
-            </Table>
-        </ScrollArea>
+                <DataTable columns={columns} data={students} />
+            </ScrollArea>
     </div> 
     );
 }
 
-function Empty() {
-    return (
-        <TableRow>
-            <TableCell colSpan={5} className="h-12 text-center">
-                No results.
-            </TableCell>
-        </TableRow>
-    );
-}
+// function Empty() {
+//     return (
+//         <TableRow>
+//             <TableCell colSpan={5} className="h-12 text-center">
+//                 No results.
+//             </TableCell>
+//         </TableRow>
+//     );
+// }
 
-function List(data: Student[]) {
-    const lines: JSX.Element[] = [];
-    data.map((row) => {
-            lines.push(
-                <TableRow key={row.name}>
-                    <TableCell>{row.id}</TableCell>
-                    <TableCell>{row.name}</TableCell>
-                    <TableCell>{row.email}</TableCell>
-                    <TableCell>{row.create_at}</TableCell>
-                </TableRow>
-        )
-    });
-    return lines;
-}
+// function List(data: Student[]) {
+//     const lines: JSX.Element[] = [];
+//     data.map((row) => {
+//             lines.push(
+//                 <TableRow key={row.name}>
+//                     <TableCell>{row.id}</TableCell>
+//                     <TableCell>{row.name}</TableCell>
+//                     <TableCell>{row.email}</TableCell>
+//                     <TableCell>{row.create_at}</TableCell>
+//                 </TableRow>
+//         )
+//     });
+//     return lines;
+// }
