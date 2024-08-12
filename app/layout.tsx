8d52fa85b9d1df18fbf7f5586312a106c269b136
facsert <facsert@outlook.com>
2024-08-12 21:03:06 +0800

    react
    1.modify dark mode
    
    Signed-off-by: facsert <facsert@outlook.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,34 +2,41 @@
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
 import Menu from "@/components/Menu";
-import useTheme from "@/hook/theme"
-import { Toaster } from "@/components/ui/sonner"
 
+import { Toaster } from "@/components/ui/sonner";
 import { Separator } from "@/components/ui/separator";
+
+import { ThemeProvider } from "@/components/theme";
 
 // bg-muted/90
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
-  const darkTheme = useTheme(state => state.dark);
   return (
-    <html lang="en" className={darkTheme? "dark": "light"}>
+    <html lang="en">
       <body className="flex flex-col w-screen h-screen bg-muted/100"> 
-        <div className="w-full h-[6vh] bg-card">
-          <Menu />
-        </div>
-        <Separator />
-        <div className="flex flex-row w-full h-[88vh]">
-          <div className="flex flex-row h-[88vh] w-[260px]">
-            <Sidebar /> 
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+          <div className="w-full h-[6vh] bg-card">
+            <Menu />
           </div>
-          <Separator orientation="vertical" />
-          <div className="h-[88vh] w-full p-4">
-            {children}
-            <Toaster position="top-center" />
+          <Separator />
+          <div className="flex flex-row w-full h-[88vh]">
+            <div className="flex flex-row h-[88vh] w-[260px]">
+              <Sidebar /> 
+            </div>
+            <Separator orientation="vertical" />
+            <div className="h-[88vh] w-full p-4">
+              {children}
+              <Toaster position="top-center" />
+            </div>
           </div>
-        </div>
-        <Separator />
-        <div className="w-full h-[6vh] bg-card">
-        </div>
+          <Separator />
+          <div className="w-full h-[6vh] bg-card">
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,26 +1,46 @@
-import { Label } from "@/components/ui/label";
-import { Button } from "@/components/ui/button";
-import { Switch } from "@/components/ui/switch";
-import useTheme from "@/hook/theme";
-
+"use client"
+ 
+import * as React from "react"
+import { MoonIcon, SunIcon } from "lucide-react"
+import { useTheme } from "next-themes"
+ 
+import { Button } from "@/components/ui/button"
 import {
-    SunMoon,
-    MoonStar,
-} from "lucide-react";
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
-export default function Menu() {
-    const switchTheme: () => void = useTheme((state) => state.switch);
-    const dark: boolean = useTheme((state) => state.dark);
-    return (
-        <div className="h-full flex flex-row justify-between items-center px-10">
-            <div>
-                <h1 className="text-3xl font-bold">Menu</h1>
-            </div>
-            <div className="flex items-center space-x-2">
-                <Button variant="link" size='icon' onClick={switchTheme}>
-                    {dark? <SunMoon/> : <MoonStar/>}
-                </Button>
-            </div>
-        </div>
-    );
+export default function Navbar() {
+  const { setTheme } = useTheme()
+  return (
+    <div className="h-full w-full flex flex-row justify-between items-center px-10">
+      <div>
+        <h1 className="text-3xl font-bold">Menu</h1>
+      </div>
+      <div className="flex items-center space-x-2">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+                <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+                Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+                Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+                System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
+  );
 };
