d723c6fa08f2680f7155e8091cb843954d3a0911
facsert <facsert@outlook.com>
2024-09-03 21:25:07 +0800

    react
    1.modify react table format
    
    Signed-off-by: facsert <facsert@outlook.com>

--- app/(components)/table/columns.tsx
+++ app/(components)/table/columns.tsx
@@ -17,88 +17,87 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 
 export type Student = {
-    id: number;
-    name: string;
-    age: number;
-    sex: "boy"|"girl";
-    create_at: string;
+  id: number;
+  name: string;
+  age: number;
+  sex: "boy"|"girl";
+  create_at: string;
 }
 
 // 定义列的标题和内容的样式
 export const columns: ColumnDef<Student>[] = [
-    {
-        accessorKey: "name",
-        header: () => <div className="text-left">Name</div>,
-        cell: ({ row }) => <div className="text-left font-medium">{row.getValue("name")}</div>,
+  {
+    accessorKey: "name",
+    header: () => <div className="text-left">Name</div>,
+    cell: ({ row }) => <div className="text-left font-medium">{row.getValue("name")}</div>,
+  },
+  {
+    accessorKey: "age",
+    cell: ({ row }) => <div className="text-left px-4">{row.getValue("age")}</div>,
+    header: ({column}) => {
+      return (
+        <div>
+          <Button
+            variant="ghost"
+            className="cursor-pointer w-full flex flex-row justify-start items-center p-2"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >   
+            <p>Age</p>
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        </div>
+      )
     },
-    {
-        accessorKey: "age",
-        header: ({column}) => {
-            return (
-                <div>
-                    <Button
-                        variant="ghost"
-                        className="cursor-pointer w-full flex flex-row justify-start items-center p-2"
-                        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-                    >   
-                        <p>Age</p>
-                        <ArrowUpDown className="ml-2 h-4 w-4" />
-                    </Button>
-                </div>
-            )
-        },
-        cell: ({row}) => <div className="text-left px-4">{row.getValue("age")}</div>
-    },
-    {
-        accessorKey: "sex",
-        header: "性别",
-        cell: ({ row }) => <div className="text-left">{row.getValue("sex") === "boy" ? "男生" : "女生"}</div>,
-    },
-    {
-        accessorKey: "create_at",
-        header: "Create At",
-    },
-    {
-        id: "actions",
-        cell: ({ row }) => {
-            const student = row.original
-            return (
-                <div className="flex flex-row gap-4 ">
-                    {/* <button>Edit</button> */}
-                    <Edit />
-                    <Button variant="ghost">Delete</Button>
-                </div>
-            )
-        }
+  },
+  {
+    accessorKey: "sex",
+    header: "性别",
+    cell: ({ row }) => <div className="text-left">{row.getValue("sex") === "boy" ? "男生" : "女生"}</div>,
+  },
+  {
+    accessorKey: "create_at",
+    header: "Create At",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const student = row.original
+      return (
+        <div className="flex flex-row gap-4 ">
+          {/* <button>Edit</button> */}
+          <Edit />
+          <Button variant="ghost">Delete</Button>
+        </div>
+      )
     }
+  }
 ]
 
 function Edit() {
     return (
-        <div className="h-10 px-4 py-2 hover:bg-accent rounded-full">
-            <Dialog>
-                <DialogTrigger className="w-full h-full flex flex-row justify-center items-center">
-                    <p>Edit</p>
-                </DialogTrigger>
-                <DialogContent>
-                    <DialogHeader>
-                        <DialogTitle>Edit</DialogTitle>
-                        <DialogDescription>
-                            Edit the student information
-                        </DialogDescription>
-                    </DialogHeader>
-                    <div>
-                        <Label htmlFor="name">name</Label>
-                        <Input id="name"></Input>
-
-                        <Label htmlFor="age">age</Label>
-                        <Input id="age"></Input>
-                    </div>
-                    <DialogClose>
-                        Edit
-                    </DialogClose>
-                </DialogContent>
-            </Dialog>
-        </div>
+      <div className="h-10 px-4 py-2 hover:bg-accent rounded-full">
+        <Dialog>
+          <DialogTrigger className="w-full h-full flex flex-row justify-center items-center">
+            <p>Edit</p>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Edit</DialogTitle>
+              <DialogDescription>
+                Edit the student information
+              </DialogDescription>
+            </DialogHeader>
+            <div>
+              <Label htmlFor="name">name</Label>
+              <Input id="name"></Input>
+              <Label htmlFor="age">age</Label>
+              <Input id="age"></Input>
+            </div>
+            <DialogClose>
+              Edit
+            </DialogClose>
+          </DialogContent>
+        </Dialog>
+      </div>
     )
 }
--- app/(components)/table/page.tsx
+++ app/(components)/table/page.tsx
@@ -1,12 +1,4 @@
 "use client"
-import {
-    Table,
-    TableBody,
-    TableHead,
-    TableHeader,
-    TableRow,
-    TableCell,
-} from "@/components/ui/table";
 
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { DataTable } from "./table";
@@ -14,70 +6,44 @@ import { columns, Student } from "./columns";
 
 const students: Student[] = [
     {
-        id: 1,
-        name: "John Doe",
-        age: 20,
-        sex: "boy",
-        create_at: "20240512",
+      id: 1,
+      name: "John Doe",
+      age: 20,
+      sex: "boy",
+      create_at: "20240512",
     },
     {
-        id: 2,
-        name: "Jane Doe",
-        age: 21,
-        sex: "girl",
-        create_at: "20240512",
+      id: 2,
+      name: "Jane Doe",
+      age: 21,
+      sex: "girl",
+      create_at: "20240512",
     },
     {
-        id: 3,
-        name: "Jack Doe",
-        age: 19,
-        sex: "boy",
-        create_at: "20240512",
+      id: 3,
+      name: "Jack Doe",
+      age: 19,
+      sex: "boy",
+      create_at: "20240512",
     },
     {
-        id: 3,
-        name: "Lily Doe",
-        age: 20,
-        sex: "girl",
-        create_at: "20240512",
+      id: 3,
+      name: "Lily Doe",
+      age: 20,
+      sex: "girl",
+      create_at: "20240512",
     }
 ]
 
 export default function TablePage() {
-    // const data:Student[] = [];
-    return (
-       <div className="flex flex-col justify-start items-center h-full w-full">
-            <div className="w-full h-[40px] text-center">
-                <h1>Student Table</h1>
-            </div>
-            <ScrollArea className="w-full h-5/6 rounded-md border">
-                <DataTable columns={columns} data={students} />
-            </ScrollArea>
-    </div> 
-    );
+  return (
+    <div className="flex flex-col justify-start items-center h-full w-full">
+      <div className="w-full h-[40px] text-center">
+        <h1>Student Table</h1>
+      </div>
+      <ScrollArea className="w-full h-5/6 rounded-md border">
+        <DataTable columns={columns} data={students} />
+      </ScrollArea>
+    </div>
+  );
 }
-
-// function Empty() {
-//     return (
-//         <TableRow>
-//             <TableCell colSpan={5} className="h-12 text-center">
-//                 No results.
-//             </TableCell>
-//         </TableRow>
-//     );
-// }
-
-// function List(data: Student[]) {
-//     const lines: JSX.Element[] = [];
-//     data.map((row) => {
-//             lines.push(
-//                 <TableRow key={row.name}>
-//                     <TableCell>{row.id}</TableCell>
-//                     <TableCell>{row.name}</TableCell>
-//                     <TableCell>{row.email}</TableCell>
-//                     <TableCell>{row.create_at}</TableCell>
-//                 </TableRow>
-//         )
-//     });
-//     return lines;
-// }
