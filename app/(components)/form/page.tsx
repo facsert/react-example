6ce8aa3926e9f12408ebddcc3383f552233f6129
facsert <facsert@outlook.com>
2024-11-20 21:44:29 +0800

    react
    1.form add radio and checkbox
    
    Signed-off-by: facsert <facsert@outlook.com>

--- app/(components)/form/page.tsx
+++ app/(components)/form/page.tsx
@@ -6,7 +6,7 @@ import { Button } from "@/components/ui/button";
 import { Select } from "@/components/ui/select";
 import { Checkbox } from "@/components/ui/checkbox";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
-import { FormEvent, useState, ChangeEvent } from "react";
+import { FormEvent, useState, ChangeEvent, FormEventHandler } from "react";
 import { toast } from "sonner"
 import ReactForm from "./react-form";
 import { set } from "react-hook-form";
@@ -22,7 +22,7 @@ interface Client {
 export default function FormPage() {
   // TODO: 添加 Form 组件, 添加下拉, 输入框, checkbox 组件
   return (
-    <div className="w-full h-full flex flex-row items-center justify-center">
+    <div className="w-full h-full flex flex-row items-center justify-center gap-2">
       <ReactForm1 />
       <ReactForm2 />
     </div>
@@ -49,10 +49,9 @@ function ReactForm1({...props}) {
   }
 
   const handleReset = () => setClient(defaultClient);
-
   return (
-    <div className="border rounded-md p-4 w-full max-w-md gap-4" {...props}>
-      <form action={handleSubmit}>
+    <div className="border rounded-md p-4 w-full max-w-md" {...props}>
+      <form action={handleSubmit} className="flex flex-col gap-4">
         <div>
           <Label htmlFor="host">Host</Label>
           <Input 
@@ -81,6 +80,7 @@ function ReactForm1({...props}) {
             name='username'
             type="text"
             id="username"
+            value={client.username}
             defaultValue={client.username}
             onChange={(e) => setClient(preState => ({...preState, username: e.target.value}))}
           />
@@ -91,6 +91,7 @@ function ReactForm1({...props}) {
             name='password'
             type="text"
             id="password"
+            value={client.password}
             defaultValue={client.password}
             onChange={(e) => setClient(preState => ({...preState, password: e.target.value}))}
           />
@@ -117,31 +118,36 @@ function ReactForm2({...props}) {
     name: string;
     age: number;
     sex: 'boy' | 'girl';
-    hobby: 'walk'| 'swim'|'ride'
+    hobby: ('walk'| 'swim'|'ride')[]
   }
   const defaultData: Person = {
-    hobby: "walk",
-    sex: 'boy',
-    age: 18,
     name: 'John',
+    age: 18,
+    sex: 'boy',
+    hobby: ["walk"],
   }
 
   const [person, setPerson] = useState<Person>(defaultData);
-  async function handleSubmit(formData: FormData) {
-    // const port: number = (formData.get('port')?? client.port) as number;
-    // setClient(preState => ({...preState,
-    //   host: formData.get('host') as string,
-    //   port: (formData.get('port') ?? client.port) as number,
-    //   username: formData.get('username') as string,
-    //   password: formData.get('password') as string,
-    // }))
+  const handleChange = (event: ChangeEvent<HTMLSelectElement | HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setPerson(preState => ({...preState, [name]: value }));
+  };
+
+  const handleCheckbox = (checked: boolean, value: 'walk'| 'swim'|'ride') => {
+    if (checked) {
+      setPerson(preState => ({...preState, hobby: [...preState.hobby, value] }));
+    } else {
+      setPerson(preState => ({...preState, hobby: preState.hobby.filter(item => item !== value) }));
+    }
+  };
+
+  const handleSubmit = (formData: FormData) => {
     toast.success(JSON.stringify(person));
   }
-  
   const handleReset = () => setPerson(defaultData);
   return (
-    <div className="border rounded-md p-4 w-full max-w-md gap-4" {...props}>
-      <form action={handleSubmit}>
+    <div className="border rounded-md p-4 w-full max-w-md" {...props}>
+      <form action={handleSubmit} className="flex flex-col gap-4">
         <div>
           <Label htmlFor="name">Name</Label>
           <Input 
@@ -149,7 +155,7 @@ function ReactForm2({...props}) {
             type="text" 
             id="name" 
             defaultValue={person.name}
-            onChange={(e) => setPerson(preState => ({...preState, name: e.target.value}))}
+            onChange={handleChange}
             value={person.name}
           />
         </div>
@@ -160,26 +166,42 @@ function ReactForm2({...props}) {
             type="number" 
             id="age" 
             defaultValue={person.age}
-            onChange={(e) => setPerson(preState => ({...preState, age: e.target.valueAsNumber}))}
+            onChange={handleChange}
             value={person.age}
           />
         </div>
-        <RadioGroup defaultValue="boy" onValueChange={(value) => console.log(value)}>
+        <RadioGroup defaultValue="boy" onValueChange={(value: 'boy' | 'girl') => setPerson(preState => ({...preState, sex: value}))}>
           <div className="flex items-center space-x-2">
-            <RadioGroupItem id="boy" value="boy" />
+            <RadioGroupItem id="boy" value="boy" checked={person.sex === 'boy'} />
             <Label htmlFor="boy">Boy</Label>
           </div>
           <div className="flex items-center space-x-2">
-            <RadioGroupItem id="girl" value="girl" />
+            <RadioGroupItem id="girl" value="girl" checked={person.sex === 'girl'} />
             <Label htmlFor="girl">Girl</Label>
           </div>
         </RadioGroup>
-        <div>
-          <Checkbox id="swim" name="hobby" value="swim" />
+        <div className="flex flex-row gap-2">
+          <Checkbox id="swim" name="hobby"
+            value="swim"
+            checked={person.hobby.includes('swim')}
+            onCheckedChange={(checked: boolean) => handleCheckbox(checked, 'swim')}
+          />
           <Label htmlFor="swim">Swim</Label>
-          <Checkbox id="ride" name="hobby" value="ride" />
+          <Checkbox
+            id="ride"
+            name="hobby"
+            value="ride"
+            checked={person.hobby.includes('ride')}
+            onCheckedChange={(checked: boolean) => handleCheckbox(checked, 'ride')}
+          />
           <Label htmlFor="ride">Ride</Label>
-          <Checkbox id="walk" name="hobby" value="walk" />
+          <Checkbox
+            id="walk"
+            name="hobby"
+            value="walk"
+            checked={person.hobby.includes('walk')}
+            onCheckedChange={(checked: boolean) => handleCheckbox(checked, 'walk')}
+          />
           <Label htmlFor="walk">Walk</Label>
         </div>
         <Button type="submit">Submit</Button>
@@ -187,13 +209,13 @@ function ReactForm2({...props}) {
       </form>
       <br />
       <div>
-        Fruit: apple
+        NAME: {person.name}
         <br />
-        
+        AGE: {person.age}
         <br />
-      
+        SEX: {person.sex}
         <br />
-  
+        HOBBY: [{person.hobby.join(', ')}]
       </div>
     </div>
     );
